53f1b2d5320a331802d62b9b77cb7956c280af6c
ilham <ilhambojo@gmail.com>
2018-11-22 20:05:53 +0700

    update I

---
+++ exercise-1.js
@@ -0,0 +1,85 @@
+// Soal 1. Newton Second Law
+
+// Rumus ΣF = m * a
+
+// 1. Algorithm
+
+// 1. Simpan nilai m(massa benda(kg))
+// 2. Simpan nilai a(resultan gaya(m/s2))
+// 3. Hitung resultan gayanya, dengan rumus m * a
+// 4. Tampilkan hasil kalkulasi pada resultan gaya
+
+// 2. Pseudocode
+// Store "m" with 600
+// Store "a" with 2
+
+// CALCULATE "m" times "a"
+// SET "resultan" value with calculation result
+
+// Display "resultan"
+
+// --------------------------------------------------------------------------
+
+//Soal 2. Tahun Kabisat
+
+// 1. Algorithm
+
+// 1. Simpan nilai tahun
+// 2. Jika tahun habis dibagi 4, dan tidak habis dibagi 100
+// 3. Maka tahun tsb Tahun Kabisat
+// 4. Jika tahun habis dibagi 4, habis dibagi 100 dan habis dibagi 400
+// 5. Maka tahun tsb Tahun Kabisat
+// 6. Jika tidak ada , maka Tahun itu bukan Tahun Kabisat
+
+// 2. Psudeocode
+
+// Store "tahun" with any value 
+// IF "tahun" mod 4 equals to 0 AND "tahun" mod 100 NOT equals to 0
+//     DISPLAY "Tahun Kabisat"
+// ELSE IF "tahun" mod 4 equals to 0 AND "tahun" mod 100 equals to 0 AND "tahun" mod 400 equals to 0
+//     DISPLAY "Tahun Kabisat"
+// ELSE DISPLAY "Bukan Tahun Kabisat"
+
+
+// -------------------------------------------------------------------------
+// Soal 3. Laundry Day
+
+// 1 . Algorithm
+
+// 1. Simpan nilai pakaian
+// 2. Ketika pakaian sudah dimasukan ke dalam mesin cuci sebanyak 20 baju
+// 3. Maka mesin cuci akan dijalankan
+
+// 2. Psuedocode
+
+// Store "pakaian" to 0
+
+// WHILE "pakaian" < 20
+//     ADD "pakaian" by 1
+
+// DO "Jalankan Mesin Cuci" 
+
+// ---------------------------------------------------------------------------------
+// Soal 4. Periksa Kuku
+
+// 1. Algorithm
+
+// 1. Simpan nilai siswa 
+// 2. Ketika siswa kukunya sudah di cek sebanyak 40 siswa
+// 3. Jika Guru menemukan siswa yang kukunya panjang
+// 4. Maka Siswa akan mendapat hukuman dari Guru
+// 5. Jika Guru menemukan siswa yang kukunya pendek
+// 6. Maka Siswa akan mendapat pujian
+
+// 2. Psuedocode 
+
+// 1. Store "siswa" to 0
+// 2. Store "guru" with "Pak Guru"
+// 3. Store "kuku"
+
+// 4. WHILE "siswa" < 40
+// 5.    DISPLAY "siswa"
+// 6. IF "kuku" equals to "panjang"
+// 7.   THEN "guru" do "menghukum"
+// 8. IF "kuku" equals to "pendek"
+// 9.   THEN "guru" do "memuji"
